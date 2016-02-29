3d5180301965e393e7b8d7a55ea70cdd6a80b058
kevteg <kevin.hernandez@unet.edu.ve>
2016-02-29 16:53:02 -0430

    Iluminar el botón de conectar cuando esta seleccionado un servidor el la lista

--- interfaz_cliente.cpp
+++ interfaz_cliente.cpp
@@ -5,6 +5,12 @@ interfaz_cliente::interfaz_cliente(QWidget *parent) : QWidget(parent), ui(new Ui
     ui->setupUi(this);
     ui->ir_atras->setIcon(QIcon(QPixmap(":/imágenes/Imágenes/back.png")));
     connect(ui->ir_atras, SIGNAL(clicked(bool)), this, SLOT(atras()));
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(limpiarListaServidores()));
+    ui->conectar_servidor->setEnabled(false);
+    connect(ui->lista_servidores, SIGNAL(itemClicked(QListWidgetItem*)), this, SLOT(itemPresionado()));
+    //connect(ui->lista_servidores, SIGNAL(itemChanged(QListWidgetItem*)), this, SLOT(itemPresionado(QListWidgetItem*)));
+    timer->start(5000);
 }
 void interfaz_cliente::atras(){
     emit goInitInterface();
